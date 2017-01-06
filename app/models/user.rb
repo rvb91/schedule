0dfaf54692733e1aa8e06ce6c1fd5cbada4b1bf0
Raghav Bagla <raghav.bagal@gmail.com>
2017-01-06 13:30:45 -0500

    default type for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
+
+  validates :name, presence:true
+  validates :default_type, presence:true, inclusion: { in: %w(nanny family) }
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  # removed :recoverable,
-  validates :name, presence:true
+  # removed :recoverable
   devise :database_authenticatable, :registerable, :rememberable, :trackable, :validatable
 end
