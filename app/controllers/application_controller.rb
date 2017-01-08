85f931796f46df27104979ae97d60e5f3b02c250
Raghav Bagla <raghav.bagal@gmail.com>
2017-01-07 23:06:50 -0500

    setting the after sign in path.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,15 @@ class ApplicationController < ActionController::Base
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :default_type])
     end
+
+    def after_sign_in_path_for(resource)
+      request.env['omniauth.origin'] || stored_location_for(resource) || root_path
+
+      case resource.default_type
+      when "nanny"
+        slots_path
+      when "family"
+        nannies_path
+      end
+    end
 end
