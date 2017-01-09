23d4f5a457745835b8bf084534638da9b9a8a88e
Raghav Bagla <raghav.bagal@gmail.com>
2017-01-09 15:18:01 -0500

    view improvements

--- app/controllers/nannies_controller.rb
+++ app/controllers/nannies_controller.rb
@@ -1,13 +1,13 @@
 class NanniesController < ApplicationController
   def index
     @start_time = params[:start_time].present? ? parse_datetime(params[:start_time]) : Time.now + 1.days
-    @end_time = params[:end_time].present? ? parse_datetime(params[:end_time]) : @start_time + 12.day
+    @end_time = params[:end_time].present? ? parse_datetime(params[:end_time]) : @start_time + 10.days
 
-    @nanny_ids = Slot.where("start_time >= ? && end_time <= ?", @start_time, @end_time)
-                     .reserveable
-                     .select("slots.nanny_id").distinct
-
-    @nannies = Nanny.where(id: @nanny_ids)
+    @nannies = Nanny.joins(:slots).
+                    where("slots.start_time >= ? && slots.end_time <= ?", @start_time, @end_time).
+                    includes(:slots).
+                    order("slots.start_time ASC").
+                    merge(Slot.reserveable)
   end
 
   def show
@@ -20,7 +20,7 @@ class NanniesController < ApplicationController
     nanny = @slot.nanny
 
     if @slot.can_reserve? && @slot.reserve_for(family)
-      redirect_to nannies_path(nanny), notice: "successfully reserved slot ##{@slot.id}"
+      redirect_to nannies_path, notice: "successfully reserved #{nanny.name} from #{helpers.format_time(@slot.start_time)} to #{helpers.format_time(@slot.end_time)}"
     else
       redirect_to nannies_path(nanny), alert: "slot cannot be reserved"
     end
